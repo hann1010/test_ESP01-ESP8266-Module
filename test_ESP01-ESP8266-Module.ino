3bf8cc7584b47db2f6f6f5f5595769415e051d89
Hanna <hanna.paivasto@gmail.com>
2025-03-28 12:33:05 +0200

    first commit

--- test_ESP01-ESP8266-Module.ino
+++ test_ESP01-ESP8266-Module.ino
@@ -1,3 +1,7 @@
+/* test_ESP01-ESP8266-Module en Hanna P 2025
+For Arduino Mega 2560 board */
+/********************************************/
+
 void setup() {
   // put your setup code here, to run once:
 
