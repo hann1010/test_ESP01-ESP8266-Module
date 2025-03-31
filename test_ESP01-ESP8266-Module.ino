1b844d389572063a6c9c3741eb6d591a95818065
Hanna <hanna.paivasto@gmail.com>
2025-03-31 20:34:56 +0300

    setup commit

--- test_ESP01-ESP8266-Module.ino
+++ test_ESP01-ESP8266-Module.ino
@@ -9,6 +9,7 @@ bool stringComplete = false;  // whether the string is complete
 void setup() {
   // initialize serial:
   Serial.begin(9600);
+  Serial1.begin(9600);
   // reserve 200 bytes for the inputString:
   inputString.reserve(200);
 
@@ -31,9 +32,9 @@ void loop() {
   delay response. Multiple bytes of data may be available.
 */
 void serialEvent() {
-  while (Serial.available()) {
+  while (Serial1.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read();
+    char inChar = (char)Serial1.read();
     // add it to the inputString:
     inputString += inChar;
     // if the incoming character is a newline, set a flag so the main loop can
