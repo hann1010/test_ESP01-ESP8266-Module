93b828557cf59e6f2a612548596e39607d739d78
Hanna <hanna.paivasto@gmail.com>
2025-04-07 12:58:57 +0300

    update

--- test_ESP01-ESP8266-Module.ino
+++ test_ESP01-ESP8266-Module.ino
@@ -46,4 +46,16 @@ void serialEvent() {
       Serial.println("Serial1 read... Done");      
     }
   }
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+      Serial.println("Serial read... Done");      
+    }
+  }
 }
